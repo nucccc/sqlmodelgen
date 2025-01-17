210b47af0de9e9c330124edbe991d6bbbbb16525
nucccc <dan.dom.nuc@gmail.com>
2025-01-17 22:09:30 +0100

    removed unused ir relationship code

--- src/sqlmodelgen/ir/ir.py
+++ src/sqlmodelgen/ir/ir.py
@@ -40,28 +40,6 @@ class TableIR:
 	
 
 @dataclass
-class RelationshipIR:
-	main_table: TableIR
-	foreign_table: TableIR
-	rel_name: str | None = None
-	foreign_rel_name: str | None = None
-
-	def determine_rel_names(self):
-		# TODO: this does not guarantee that two relationships do not have the same name
-		if self.rel_name is None:
-			rel_name = self.foreign_table.name + 's'
-			# i keep adding an s until the 
-			while self.main_table.get_col_ir(rel_name) is not None:
-				rel_name = rel_name + 's'
-			self.rel_name = rel_name
-		if self.foreign_rel_name is None:
-			foreign_rel_name = self.main_table.name
-			while self.foreign_table.get_col_ir(foreign_rel_name) is not None:
-				foreign_rel_name = foreign_rel_name + 's'
-			self.foreign_rel_name = foreign_rel_name
-
-
-@dataclass
 class SchemaIR:
 	table_irs: list[TableIR]
 
@@ -75,23 +53,3 @@ class SchemaIR:
 				continue
 			return table_ir
 		return None
-
-
-	def arrange_relationships(self):
-		'''
-		this given the tables' foreign keys I hope can
-		'''
-		for table_ir in self.table_irs:
-			for col_ir in table_ir.col_irs:
-				if col_ir.foreign_key is None:
-					continue
-
-				main_table = self.get_table_ir(col_ir.foreign_key.target_table)
-
-				rel_ir = RelationshipIR(
-					main_table=main_table,
-					foreign_table=table_ir
-				)
-
-				table_ir.foreign_relationships.append(rel_ir)
-				main_table.relationships.append(rel_ir)
