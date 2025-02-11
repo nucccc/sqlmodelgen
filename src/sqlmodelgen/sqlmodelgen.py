440681809efc16886536b31ad3822d7214763c5c
nucccc <dan.dom.nuc@gmail.com>
2025-02-11 22:42:05 +0100

    implementing unit tests for foreign key generation

--- src/sqlmodelgen/sqlmodelgen.py
+++ src/sqlmodelgen/sqlmodelgen.py
@@ -2,5 +2,5 @@ from .codegen.codegen import gen_code
 from .ir.parse.ir_parse import ir_parse
 
 
-def gen_code_from_sql(sql_code: str) -> str:
-    return gen_code(ir_parse(sql_code))
+def gen_code_from_sql(sql_code: str, generate_relationships: bool = False) -> str:
+    return gen_code(ir_parse(sql_code), generate_relationships)
