eb1ea5286a1580a66a6f26fc32fb6a6931e386dc
nucccc <dan.dom.nuc@gmail.com>
2025-05-17 11:34:47 +0200

    began writing code to generate unique table args

--- src/sqlmodelgen/codegen/cir_to_full_ast/code_ir_to_ast.py
+++ src/sqlmodelgen/codegen/cir_to_full_ast/code_ir_to_ast.py
@@ -28,6 +28,11 @@ def model_ir_to_ast(model_ir: ModelIR) -> ast.ClassDef:
         )
     ]
 
+    # checking if the __table_args__ shall be added
+    table_args = gen_table_args(model_ir)
+    if table_args is not None:
+        body.append(table_args)
+
     # adding assignment lines
     for attr_ir in model_ir.iter_attrs():
         body.append(
@@ -46,6 +51,21 @@ def model_ir_to_ast(model_ir: ModelIR) -> ast.ClassDef:
             )
         ]
     )
+
+
+def gen_table_args(model_ir: ModelIR) -> ast.Assign | None:
+    if len(model_ir.table_args) == 0:
+        return None
+    
+    # at this level we gust generate the unique constraint
+    
+    return ast.Assign(
+        targets=[ast.Name('__table_args__')],
+        value=ast.List(
+            elts=[table_arg.to_expr() for table_arg in model_ir.table_args]
+        )
+    )
+
 
 
 def attr_to_ast(attr_ir: AttributeIR) -> ast.AnnAssign:
