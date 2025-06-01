d5c5dfef72571f34907280eaeedea19aea7ba95f
nucccc <dan.dom.nuc@gmail.com>
2025-06-01 19:33:03 +0200

    testing for behaviours with table name formats previous to sqloxide 0.1.56

--- tests/test_ir_parse.py
+++ tests/test_ir_parse.py
@@ -1,4 +1,20 @@
-from sqlmodelgen.ir.parse.ir_parse import collect_data_type
+from sqlmodelgen.ir.parse.ir_parse import (
+    table_name_from_ctparsed,
+    collect_data_type
+)
+
+
+def test_table_name_from_ctparsed():
+    # testing for ct_parsed formats, either before and after sqloxide
+    # version 0.1.56 introducing Identifier
+    assert table_name_from_ctparsed(
+        {'name':[{'value':'hero'}]}
+    ) == 'hero'
+
+    assert table_name_from_ctparsed(
+        {'name':[{'Identifier':{'value':'hero'}}]}
+    ) == 'hero'
+
 
 def test_collect_data_type():
     assert collect_data_type('Text') == 'Text'
