0b6a4cc52e1c8e2a8672151a24fffb052ca5899b
nucccc <dan.dom.nuc@gmail.com>
2025-11-16 22:19:42 +0100

    added test for mysql foreign key relationship

--- tests/test_gen_from_mysql.py
+++ tests/test_gen_from_mysql.py
@@ -57,3 +57,55 @@ class Persons(SQLModel, table=True):
     Age: int | None'''
         
         assert collect_code_info(code) == collect_code_info(expected_code)
+
+
+def test_mysql_fk_rel():
+    with mysql_docker() as (mysqld, conn):
+        cur = conn.cursor()
+
+        sqls = ['''CREATE TABLE nations(
+            id INT PRIMARY KEY,
+            name TEXT NOT NULL
+        );''',
+        '''CREATE TABLE athletes(
+            id INT PRIMARY KEY,
+            name TEXT NOT NULL,
+            nation_id INT,
+            FOREIGN KEY (nation_id) REFERENCES nations(id),
+            height INTEGER,
+            weight INTEGER,
+            bio TEXT,
+            nickname TEXT
+        );''']
+
+        cur.execute('CREATE DATABASE IF NOT EXISTS nucdb')
+
+        cur.execute('USE nucdb')
+
+        for sql in sqls:
+            cur.execute(sql)
+
+        conn.commit()
+
+        code = gen_code_from_mysql(conn, 'nucdb')
+
+        print(code)
+
+        expected_code ='''from sqlmodel import SQLModel, Field
+
+class Athletes(SQLModel, table=True):
+    __tablename__ = 'athletes'
+    id: int = Field(primary_key=True)
+    name: str
+    nation_id: int | None = Field(foreign_key='nations.id')
+    height: int | None
+    weight: int | None
+    bio: str | None
+    nickname: str | None
+
+class Nations(SQLModel, table=True):
+    __tablename__ = 'nations'
+    id: int = Field(primary_key=True)
+    name: str'''
+        
+        assert collect_code_info(code) == collect_code_info(expected_code)
