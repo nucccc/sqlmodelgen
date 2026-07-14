79664c06c529915731963edde32319b565b12c1d
Joaquim Esteves <jesteves@pictet-technologies.com>
2026-07-14 13:58:17 +0200

    ✨ Adds `add-header` flag!
    If the `--add-header` flag is passed then sqlmodelgen will include a
    header at the top of the file that looks like so:
    
    ```python
    ```
    
    Version comes from the `importlib.metadata.version('sqlmodelgen')`
    Timestamp is localtime converted to UTC.
    
    > But y doe?
    
    Whenever I commit auto-generated files I want it to be visible that this
    was created by a program; lest future programmers think to edit them
    manually. This little flag saves me from having to create a bash
    monstrosity.

--- src/sqlmodelgen/__init__.py
+++ src/sqlmodelgen/__init__.py
@@ -1,16 +1,17 @@
+import importlib.metadata
+
 from .sqlmodelgen import (
-    gen_code_from_sql,
-    gen_code_from_sqlite,
+    gen_code_from_sql as gen_code_from_sql,
+    gen_code_from_sqlite as gen_code_from_sqlite,
 )
 
 from .utils.dependency_checker import check_postgres_deps, check_mysql_deps
 
 if check_postgres_deps():
-    from .sqlmodelgen import (
-        gen_code_from_postgres
-    )
+    from .sqlmodelgen import gen_code_from_postgres as gen_code_from_postgres
 
 if check_mysql_deps():
-    from .sqlmodelgen import (
-        gen_code_from_mysql
-    )
+    from .sqlmodelgen import gen_code_from_mysql as gen_code_from_mysql
+
+
+__version__ = importlib.metadata.version('sqlmodelgen')
