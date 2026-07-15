f2457da4d61d628ddb7d31326c816b4d5354abda
Joaquim Esteves <jesteves@pictet-technologies.com>
2026-07-15 11:02:28 +0200

    ✂️ No more argv on cli.__init__.py

--- src/sqlmodelgen/__main__.py
+++ src/sqlmodelgen/__main__.py
@@ -1,5 +1,10 @@
 from sqlmodelgen.cli import main_cli
 
+def main():
+    import sys
+    # Tweaks argv so `/Users/user/...etc/sqlmodelgen` is not shown
+    main_cli(sys.argv[1:])
+
 
 if __name__ == '__main__':
-    main_cli()
+    main()
